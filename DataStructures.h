286f968736c272b6b0befa09db5ec5ca55978a2d
ereyec <eduardo.reyes.103104@gmail.com>
2025-01-19 23:41:21 -0600

    First commit

---
+++ DataStructures.h
@@ -0,0 +1,8 @@
+bool breadboardOccupancyMatrixF[59][7];
+bool breadboardOccupancyMatrixA[59][7];
+
+int breadboardRailDescriptorArrayF[59];
+int breadboardRailDescriptorArrayA[59];
+
+//Initialize BOM F and BOM A properly. 
+
